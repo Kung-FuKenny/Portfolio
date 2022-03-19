b32cc242c3ee6d4f186686da9b6599cc2b347321
KENNY_NGOIE <mukonkolekngoie@gmail.com>
2022-03-19 10:11:54 +0200

    update pic

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
-    title: "Who I am",
+    title: "Me",
     description1: "My name is Kenny. I'm a graphic/web designer and developer based in Johannesburg, South Africa.",
-    description2: "I  work on freelance projects and utilize some spare time to build my own products. I spent my leisure hours going to church, playing musical instruments and playing basketball. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas so I can be able to help communities in the future.",
+    description2: "I  work on freelance projects and utilize some spare time to build my own products. I spent my leisure hours going to church, playing basketball and being overly obsessed with music. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new things so I can be able to help communities solve problems. We have to start somewhere :-) .",
     image: 2
 }
