d4d45f29102f860849e65fcb1b6d95d553df6e89
KENNY_NGOIE <mukonkolekngoie@gmail.com>
2022-03-19 13:52:17 +0200

    Errors

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,8 +3,8 @@ import kenny from '../assets/pdf/kenny.jpg'
 
 export const headerData = {
     name: 'Kenny M. Ngoie',
-    title: "Graphic Designer & Web Developer",
-    desciption:"God has made everything beautiful in its time. He has also set eternity in the human heart; yet[a] no one can fathom what God has done from beginning to end.",
+    title: "Graphic & Web Designer",
+    desciption:"God has made everything beautiful in its time. He has also set eternity in the human heart; yet no one can fathom what God has done from beginning to end.",
     image: kenny,
     resumePdf: resume
 }
